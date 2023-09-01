4036b152305dd0e4fa9aca7f47e2c3424f86a61c
System250823 <danshvets.ua@gmail.com>
2023-09-01 13:02:03 +0300

    First init finished bot

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
-print('hello world')
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
 
-print('Це взлмощикии')
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '6152105477:AAGXcc_qWcv3CwpgO2AFQ0jI3CKkVQldORc'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Привіт це перший бот на Python для телеграму!\n Зроблений HILLEL IT SCHOOL\n\n Напиши мені щось і я повторю твої слова!")
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
